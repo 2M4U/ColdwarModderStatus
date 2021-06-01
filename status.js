e61c08e004b7391b89898e23105bca199790e9e0
RudementalHack <cybercdn.host@gmail.com>
2021-06-01 10:09:00 +0100

    Update status.js

--- status.js
+++ status.js
@@ -2,7 +2,7 @@ const { loopStatus } = new (require('./lib/functions/requests'));
 const { log, title, clear, processes } = new (require('./lib/log/console'));
 
 clear();
-log("COLD WAR AUTO STATUS & DISCORD ADVERTISER","Developed By https://github.com/CyberCDN\n\n")
+log("COLD WAR AUTO STATUS & DISCORD ADVERTISER","Developed By https://github.com/RudementalHack/ColdwarModderStatus\n\n")
 loopStatus();
 title();
 processes();
